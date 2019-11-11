168ed9a9b9d277e5150939a40be1db82c0b11b8b
kiback2da <kiback2da@steconsulting.co.kr>
2019-11-11 21:10:19 +0900

    Adapter View 기반 작성

---
+++ app/src/main/java/com/tjoeun/a20191111_01_dateandtime/adapter/ScheduleTypeSpinnerAdapter.kt
@@ -0,0 +1,33 @@
+package com.tjoeun.a20191111_01_dateandtime.adapter
+
+import android.content.Context
+import android.text.Layout
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import com.tjoeun.a20191111_01_dateandtime.R
+import com.tjoeun.a20191111_01_dateandtime.datas.ScheduleType
+
+class ScheduleTypeSpinnerAdapter(context: Context, res:Int, list:ArrayList<ScheduleType>) : ArrayAdapter<ScheduleType>(context,res,list){
+
+    var mContext = context
+    var mList = list
+    var inf = LayoutInflater.from(mContext)
+
+    constructor(context: Context, list: ArrayList<ScheduleType>) : this(context, R.layout.schedule_type_list_item, list)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView
+        if(tempRow == null){
+            tempRow = inf.inflate(R.layout.schedule_type_list_item, null)
+        }
+
+        var row = tempRow!!
+
+
+
+        return row
+    }
+}
