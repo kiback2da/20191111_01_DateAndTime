e7498e70512fc11ff7fb77d27f390a53083cc0bc
kiback2da <kiback2da@steconsulting.co.kr>
2019-11-11 21:46:23 +0900

    상황에 맞는 데이터 출력

--- app/src/main/java/com/tjoeun/a20191111_01_dateandtime/adapter/ScheduleTypeSpinnerAdapter.kt
+++ app/src/main/java/com/tjoeun/a20191111_01_dateandtime/adapter/ScheduleTypeSpinnerAdapter.kt
@@ -1,12 +1,15 @@
 package com.tjoeun.a20191111_01_dateandtime.adapter
 
 import android.content.Context
+import android.graphics.Color
 import android.text.Layout
+import android.text.style.BackgroundColorSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.Spinner
+import android.widget.TextClock
 import android.widget.TextView
 import com.tjoeun.a20191111_01_dateandtime.R
 import com.tjoeun.a20191111_01_dateandtime.datas.ScheduleType
@@ -28,6 +31,14 @@ class ScheduleTypeSpinnerAdapter(context: Context, res:Int, list:ArrayList<Sched
 
         var row = tempRow!!
 
+        var list = mList.get(position)
+        var listTitle = row.findViewById<TextView>(R.id.listTitle)
+        var listContent = row.findViewById<TextView>(R.id.listContent)
+
+        listTitle.text = list.title.split(" ")[0]
+        listContent.text = list.title
+        listTitle.setBackgroundColor(Color.parseColor(list.color))
+
 
         return row
     }
@@ -41,8 +52,13 @@ class ScheduleTypeSpinnerAdapter(context: Context, res:Int, list:ArrayList<Sched
         var row = tempRow!!
 
         var list = mList.get(position)
+        var title = row.findViewById<TextView>(R.id.typeTitle)
+        var content = row.findViewById<TextView>(R.id.typeContent)
 
+        title.text = list.title.split(" ")[0]
+        content.text = list.title
 
+        title.setBackgroundColor(Color.parseColor(list.color))
         return row
     }
 }
